7e763491e248abf98143c6751e93427103c3a39d
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2026-02-05 20:02:54 -0300

    feat: criação de pagina unidade

--- site-loja/appU.js
+++ site-loja/appU.js
@@ -1,23 +1,30 @@
 function fnMontarCardUnidade(unidade){
     let cartao = `
         <div class="col-12 col-sm-12 col-md-6 col-lg-4 mb-3">
-                <div class="card">
+                <div class="card-unidade">
                     <img src="${unidade.foto}"
                         class="card-img-top" alt="${unidade.nome_da_loja}">
                     <div class="card-body">
                         <h5 class="card-title">${unidade.nome_da_loja}</h5>
                         <p class="card-text">${unidade.endereco}</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <span class="h5 mb-0"> ${unidade.telefone}</span>
-                        </div>
+                        <div class="contato">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <span class="h5 mb-0"> ${unidade.telefone}</span>
+                            </div>
+                            <div>
+                                <span class="h5 mb-0"> ${unidade.email}</span>
+                            </div>
                         <div>
-                            <span class="h5 mb-0"> ${unidade.email}</span>
-                        </div>
                     </div>
-                    <div class="card-footer d-flex justify-content-between bg-light">
-                        <button class="btn btn-primary btn-sm">Comprar</button>
-                        <button class="btn btn-outline-secondary btn-sm"><i class="bi bi-heart"></i></button>
-                    </div>
+                    <div class="mt-2">
+                        <a 
+                        href="https://www.google.com/maps?q=${unidade.latitude},${unidade.longitude}" 
+                        target="_blank"
+                        class="btn btn-sm btn-outline-primary"
+                        >
+                            <i class="bi bi-geo-alt"></i> Ver no mapa
+                        </a>
+                    </div>  
                 </div>
             </div>
     `

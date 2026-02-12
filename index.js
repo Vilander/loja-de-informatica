bf6cd91f4257e5f0f501d07e92671b9b3bbc3073
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2026-02-12 20:34:12 -0300

    feat: cadastro de unidade

--- index.js
+++ index.js
@@ -114,6 +114,19 @@ app.get("/unidades", function (req, res){
     })
 })
 
+app.post("/unidade/",function(req,res){
+    const { nome_da_loja, foto, telefone, email, endereco, latitude, longitude} = req.body
+    conexao.query(`INSERT INTO unidades(nome_da_loja, foto, telefone, email, endereco, latitude, longitude) values('${nome_da_loja}', '${foto}', '${telefone}', '${email}', '${endereco}', '${latitude}', '${longitude}')`,
+        function(erro, resultado){
+            if(erro){
+                res.json(erro)
+            }
+            res.send(resultado.insertId)
+        }
+    )
+})
+
+
 
 app.listen (3000)
 

2cfd7fc950ed071ac15534cd0cc715221abbb982
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2026-02-12 21:56:41 -0300

    implementado favicon

--- site-loja/app-form-unidade.js
+++ site-loja/app-form-unidade.js
@@ -1,57 +1,60 @@
 function fnAlterarFoto() {
-    if (foto.value != '') {
-        document.getElementById("fundo-imagem").style.backgroundImage = `url('${foto.value}')`
-    } else {
-        document.getElementById("fundo-imagem").style.backgroundImage = `url('https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`
-    }
-    console.log(foto.value)
+  if (foto.value != "") {
+    document.getElementById("fundo-imagem").style.backgroundImage =
+      `url('${foto.value}')`;
+  } else {
+    document.getElementById("fundo-imagem").style.backgroundImage =
+      `url('https://images.unsplash.com/photo-1740630386542-6e0953160fb4?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`;
+  }
+  console.log(foto.value);
 }
 
 function fnLimparCampos() {
-    document.getElementById("form-unidades").reset()
+  document.getElementById("form-unidades").reset();
 }
 
 function fnCadastrarUnidades() {
+  let formDados = {
+    nome_da_loja: document.getElementById("nome_da_loja").value,
+    foto: document.getElementById("foto").value,
+    telefone: document.getElementById("telefone").value,
+    email: document.getElementById("email").value,
+    endereco: document.getElementById("endereco").value,
+    latitude: document.getElementById("latitude").value,
+    longitude: document.getElementById("longitude").value,
+  };
 
-    let formDados = {
-        nome_da_loja:document.getElementById("nome_da_loja").value,
-        foto:document.getElementById("foto").value,
-        telefone:document.getElementById("telefone").value,
-        email:document.getElementById("email").value,
-        endereco:document.getElementById("endereco").value,
-        latitude:document.getElementById("latitude").value,
-        longitude:document.getElementById("longitude").value
-    }
+  console.dir(formDados);
 
-    
-
-    console.dir(formDados)
-
-    fetch('http://localhost:3000/unidade/', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(formDados)
-        
+  fetch("http://localhost:3000/unidade/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formDados),
+  })
+    .then((resposta) => resposta.json())
+    .then((dados) => {
+      fnLimparCampos();
+      console.log(dados);
     })
-    .then(resposta => resposta.json())
-    .then((dados)=>{
-        fnLimparCampos()
-        console.log(dados)
-    })
-    .catch(erro=>console.log(erro.message))
+    .catch((erro) => console.log(erro.message));
 }
 
-let foto = document.getElementById("foto")
-let btn_salvar = document.getElementById("btn-salvar-unidade")
-
+let foto = document.getElementById("foto");
+let btn_salvar = document.getElementById("btn-salvar-unidade");
 
 foto.addEventListener("blur", function () {
-    fnAlterarFoto()
-})
+  fnAlterarFoto();
+});
 
 btn_salvar.addEventListener("click", () => {
-    fnCadastrarUnidades()
-})
+  fnCadastrarUnidades();
+});
 
+const telefoneInput = document.getElementById("telefone");
 
- 
+telefoneInput.addEventListener("input", function (e) {
+  let valor = e.target.value.replace(/\D/g, "");
+  valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2");
+  valor = valor.replace(/(\d{5})(\d)/, "$1-$2");
+  e.target.value = valor;
+});
